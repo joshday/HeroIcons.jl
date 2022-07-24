b41ca4f6ee4565d309fd51d2bac7ed954d030ad9
Josh Day <emailjoshday@gmail.com>
2022-07-24 08:23:27 -0500

    rewrite and use artifacts

--- README.md
+++ README.md
@@ -1,14 +1,22 @@
 # HeroIcons
 
-This package provides access to the [https://heroicons.com](https://heroicons.com) collection of SVG icons from Julia.  
+- This package provides access to the [https://heroicons.com](https://heroicons.com) collection of SVG icons from Julia.
+- Any `HeroIcons.Icon` will display as expected in `MIME"text/html"` environments (like [Pluto](https://github.com/fonsp/Pluto.jl)!).
 
-My main main use case is adding icons to [Pluto](https://github.com/fonsp/Pluto.jl) notebooks with large UI components.
 
-
-## Usage is Simple
+## Usage
 
 ```julia
-using HeroIcons 
+using HeroIcons
 
-HeroIcon("adjustments", css="height:100px; width:100px")
+Icon("arrow-circle-down")  # outline
+
+Icon("arrow-circle-down", false)  # solid
+```
+
+- For ease of use, HeroIcons exports two NamedTuples (`outline` and `solid`) that contain every `Icon`.  This lets you auto-complete icon names.
+
+```julia
+julia> outline.arrow  # pressing tab in the REPL/Pluto will reveal matching icons.
+arrow_circle_down  arrow_circle_right  arrow_down ...
 ```
--- src/HeroIcons.jl
+++ src/HeroIcons.jl
@@ -1,33 +1,32 @@
 module HeroIcons
 
-export HeroIcon
+using Artifacts
 
-function __init__()
-    if !isdir(joinpath(@__DIR__, "..", "deps", "heroicons-0.4.2"))
-        error("HeroIcons should be rebuilt.")
-    end
-end
+export Icon, outline, solid
 
-struct HeroIcon 
+# init
+src = joinpath(artifact"HeroIcons", "heroicons-1.0.6")
+icons = map(x -> replace(x, ".svg" => ""), readdir(joinpath(src, "optimized", "outline")))
+
+outline = NamedTuple{Tuple(Symbol.(map(x -> replace(x, "-" => "_"), icons))), NTuple{length(icons), Icon}}(Icon.(icons))
+solid = map(x -> Icon(x.name, false), outline)
+
+#-----------------------------------------------------------------------------# Icon
+struct Icon
     name::String
-    style::Symbol
-    data::String
-    css::String
-    function HeroIcon(name::String; style::Symbol = :outline, css = "height:24px;width:24px;")
-        style in [:outline, :solid] || error("style must be :outline or :solid")
-        data = read(joinpath(@__DIR__, "..", "deps", "heroicons-0.4.2", "optimized", "$style", "$name.svg"), String)
-        new(name, style, data, css)
+    outline::Bool
+    function Icon(name::AbstractString, outline::Bool=true)
+        name in icons || error("Icon `$name` not found.")
+        new(name, outline)
     end
 end
-function Base.show(io::IO, o::HeroIcon)
-    println(io, "HeroIcon: \"$(o.name)\"")
-    println(io, "  - style: ", o.style)
-    println(io, "  - css:   ", o.css)
-end
 
-
-function Base.show(io::IO, ::MIME"text/html", o::HeroIcon)
-    show(io, MIME"text/html"(), HTML("""<div style="$(o.css)")>$(o.data)</div>"""))
+function Base.show(io::IO, ::MIME"text/html", o::Icon)
+    sz = o.outline ? 24 : 20
+    print(io, """<div style="height: $(sz)px; width: $(sz)px;">""")
+    show(io, MIME"text/html"(), HTML(data(o)))
+    print(io, "</div>")
 end
+data(o::Icon) = read(joinpath(src, "optimized", o.outline ? "outline" : "solid", o.name * ".svg"), String)
 
 end
